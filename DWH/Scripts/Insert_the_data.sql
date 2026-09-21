fd3c124263531b6d7cdcca77c955578e9c7976c7
md-habibullah-99 <habibullahhisam@gmail.com>
2026-09-21 20:21:22 +0600

    Add try catch.

--- DWH/Scripts/Insert_the_data.sql
+++ DWH/Scripts/Insert_the_data.sql
@@ -20,95 +20,105 @@ WITH (
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	PRINT '=============================================='
-	PRINT 'Loading Bronze Layer'
-	PRINT '=============================================='
+	BEGIN TRY
+		PRINT '=============================================='
+		PRINT 'Loading Bronze Layer'
+		PRINT '=============================================='
+		
+		PRINT '------------------------------'
+		PRINT 'Loading crm table'
+		PRINT '------------------------------'
+		
+		PRINT '-> Truncating Table: bronze.crm_cust_info'
+		TRUNCATE TABLE bronze.crm_cust_info;
 	
-	PRINT '------------------------------'
-	PRINT 'Loading crm table'
-	PRINT '------------------------------'
+		PRINT '-> Inserting data into: bronze.crm_cust_info'
+		BULK INSERT bronze.crm_cust_info
+		FROM '/tmp/cust_info.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
 	
-	PRINT '-> Truncating Table: bronze.crm_cust_info'
-	TRUNCATE TABLE bronze.crm_cust_info;
-
-	PRINT '-> Inserting data into: bronze.crm_cust_info'
-	BULK INSERT bronze.crm_cust_info
-	FROM '/tmp/cust_info.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
-
-	PRINT '-> Truncating Table: bronze.crm_prd_info'
-	TRUNCATE TABLE bronze.crm_prd_info;
-
-	PRINT '-> Inserting data into: bronze.crm_prd_info'
-	BULK INSERT bronze.crm_prd_info
-	FROM '/tmp/prd_info.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
+		PRINT '-> Truncating Table: bronze.crm_prd_info'
+		TRUNCATE TABLE bronze.crm_prd_info;
 	
-	PRINT '-> Truncating Table: bronze.crm_sales_details'
-	TRUNCATE TABLE bronze.crm_sales_details;
-
-	PRINT '-> Inserting data into: bronze.crm_sales_details'
-	BULK INSERT bronze.crm_sales_details
-	FROM '/tmp/sales_details.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
+		PRINT '-> Inserting data into: bronze.crm_prd_info'
+		BULK INSERT bronze.crm_prd_info
+		FROM '/tmp/prd_info.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
+		
+		PRINT '-> Truncating Table: bronze.crm_sales_details'
+		TRUNCATE TABLE bronze.crm_sales_details;
 	
-	PRINT '------------------------------'
-	PRINT 'Loading erp tables'
-	PRINT '------------------------------'
+		PRINT '-> Inserting data into: bronze.crm_sales_details'
+		BULK INSERT bronze.crm_sales_details
+		FROM '/tmp/sales_details.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
+		
+		PRINT '------------------------------'
+		PRINT 'Loading erp tables'
+		PRINT '------------------------------'
+		
+		PRINT '-> Truncating Table: bronze.erp_cust_az12'	
+		TRUNCATE TABLE bronze.erp_cust_az12;
 	
-	PRINT '-> Truncating Table: bronze.erp_cust_az12'	
-	TRUNCATE TABLE bronze.erp_cust_az12;
-
-	PRINT '-> Inserting data into: bronze.erp_cust_az12'
-	BULK INSERT bronze.erp_cust_az12
-	FROM '/tmp/CUST_AZ12.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
+		PRINT '-> Inserting data into: bronze.erp_cust_az12'
+		BULK INSERT bronze.erp_cust_az12
+		FROM '/tmp/CUST_AZ12.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
+		
+		PRINT '-> Truncating Table: bronze.erp_loc_a101'
+		TRUNCATE TABLE bronze.erp_loc_a101;
 	
-	PRINT '-> Truncating Table: bronze.erp_loc_a101'
-	TRUNCATE TABLE bronze.erp_loc_a101;
-
-	PRINT '-> Inserting data into: bronze.erp_loc_a101'
-	BULK INSERT bronze.erp_loc_a101
-	FROM '/tmp/LOC_A101.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
+		PRINT '-> Inserting data into: bronze.erp_loc_a101'
+		BULK INSERT bronze.erp_loc_a101
+		FROM '/tmp/LOC_A101.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
+		
+		PRINT '-> Truncating Table: bronze.erp_px_cat_g1v2'
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 	
-	PRINT '-> Truncating Table: bronze.erp_px_cat_g1v2'
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-	PRINT '-> Inserting data into: bronze.erp_px_cat_g1v2'
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM '/tmp/PX_CAT_G1V2.csv'
-	WITH (
-	    FIRSTROW = 2,
-	    FIELDTERMINATOR = ',',
-	    ROWTERMINATOR = '\n',
-	    TABLOCK
-	);
+		PRINT '-> Inserting data into: bronze.erp_px_cat_g1v2'
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM '/tmp/PX_CAT_G1V2.csv'
+		WITH (
+		    FIRSTROW = 2,
+		    FIELDTERMINATOR = ',',
+		    ROWTERMINATOR = '\n',
+		    TABLOCK
+		);
+	END TRY
+	BEGIN CATCH
+		PRINT '===================================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Number' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error State' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '===================================================='
+	END CATCH
 END;
 
 
